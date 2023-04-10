d8c033b75d438d5cd7ba664ee0b50c09dcc1d3c5
James Crutchley <jamescrutchley98@gmail.com>
2023-04-10 20:26:11 +1000

    split code and fix issue with editing items

---
+++ src/edit/editItem.js
@@ -0,0 +1,24 @@
+import itemCollection from "../manageCollection";
+
+const editItemMsgInput = document.querySelector('#edit-message');
+const editItemDateInput = document.querySelector('#edit-date');
+const editItemForm = document.querySelector('#edit-item-form');
+
+
+let selectedItem = null;
+
+const editItem = function(id) {
+    selectedItem = itemCollection.collection.find(item => item.id == id);
+    console.log('item selected: ' + selectedItem);
+    editItemMsgInput.value = selectedItem.msg;
+    editItemDateInput.value = selectedItem.date;
+    console.log('the selected item is: ' + selectedItem + id);
+    editItemForm.style.display = 'block';
+};
+
+const confirmEditItem = () => {
+    let messageInput = editItemMsgInput.value;
+    itemCollection.modifyObject(selectedItem, messageInput);
+};
+
+export { editItem, confirmEditItem };
